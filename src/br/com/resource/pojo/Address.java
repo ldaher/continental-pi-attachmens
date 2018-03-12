b6643f0772c518c45f7a93d16ed7ceb7c1ad8a90
Luciano Daher <luciano_daher@apple.com>
2018-03-12 10:03:48 -0300

    adicionando campos ao pojo Address;

--- src/br/com/resource/pojo/Address.java
+++ src/br/com/resource/pojo/Address.java
@@ -1,5 +1,59 @@
 package br.com.resource.pojo;
 
 public class Address {
+	private String addressType;
+	private String address;
+	private Integer number;
+	private String neighborhood;
+	private String city;
+	private String state;
+
+	public String getAddressType() {
+		return addressType;
+	}
+
+	public void setAddressType(String addressType) {
+		this.addressType = addressType;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
 
 }
