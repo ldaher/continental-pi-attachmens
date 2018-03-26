f5ccc7df8e6d027398c9da4e5e5d5b875fcc800f
ITLINFOSYS\luciano.daher <luciano.daher@infosys.com>
2018-03-26 14:25:26 -0300

    adicionando suppresswarnings("unused")

--- src/br/com/resource/app/LerXmlApp.java
+++ src/br/com/resource/app/LerXmlApp.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 
 import br.com.resource.attachments.ReadAttachmentsNFe10_split;
 
+@SuppressWarnings("unused")
 public class LerXmlApp {
 	public static void main(String[] args) {
 		byte[] byteBuf = new byte[4096];
